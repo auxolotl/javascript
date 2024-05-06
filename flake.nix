3804fb2a52ce7316c9a885132d41e4865b978c03
Ruby Iris Juric <ruby@srxl.me>
2024-05-06 23:49:28 +1000

    nodejs: create package set with packagesFromDirectoryRecursive

--- flake.nix
+++ flake.nix
@@ -20,13 +20,9 @@
       packages = forAllSystems (
         { nixPackages, ... }:
         {
-          nodejs = rec {
-            v18 = nixPackages.callPackage ./runtimes/nodejs/versions/v18.nix { };
-            v20 = nixPackages.callPackage ./runtimes/nodejs/versions/v20.nix { };
-            v22 = nixPackages.callPackage ./runtimes/nodejs/versions/v22.nix { };
-
-            lts = v20;
-            latest = v22;
+          nodejs = import ./runtimes/nodejs/packages.nix {
+            inherit (core.lib) packagesFromDirectoryRecursive;
+            inherit (nixPackages) callPackage;
           };
         }
       );
